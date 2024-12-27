361f96915befcbc8660b85a5fcb099098e06ebb4
Emanuel Gomes <emanuelgomes21121@outlook.com>
2024-12-27 16:18:17 +0000

    C00

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+    char numbers;
+
+    numbers = '0';
+    while(numbers <= '9')
+    {
+        write(1, &numbers, 1);
+        numbers++;
+    }
+}
+/*
+int main()
+{
+    ft_print_numbers();
+    return (0);
+}
+*/
